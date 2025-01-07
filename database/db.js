89a0e28d21c1b3f3343087d97b7d6c5ea5560301
fahrihostingg <150719442+fahrihostingg@users.noreply.github.com>
2025-01-08 02:36:39 +0800

    Add files via upload

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://api.sekha.me';
+var url = 'https://api.fakrulafif.web.id';
 try {
-if(url == 'https://api.sekha.me') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://api.fakrulafif.web.id') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
